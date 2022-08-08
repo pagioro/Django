d9c9bc4b192866fd5d37ed3ec9e7824261e7e90f
pagioro <pagioro@hotmail.com>
2022-08-08 20:11:07 +0000

    create get_todo_list function and folders template and todo with todo_list.html

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,5 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render
 # Create your views here.
-# print("hello!")
 
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
